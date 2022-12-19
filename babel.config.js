de27bff8edbb77809f2fece0bffb8d69e39d6b59
pietroanello <pietro.anello@hotmail.it>
2022-12-19 17:29:23 +0100

    folder structure helpers

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@context': './src/context',
+            '@HOC': './src/HOC',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@screens': './src/screens',
+            '@svgs': './src/svgs',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  };
 };
